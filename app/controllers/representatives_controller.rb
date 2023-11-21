d02ad383c81c985c03ebcfc2d0d631caa72cd832
Jazem Siddiqui <jazem@berkeley.edu>
2023-11-21 00:30:12 +0000

    finish profile page implementation

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -7,5 +7,13 @@ class RepresentativesController < ApplicationController
 
   def show
     @representative = Representative.find(params[:id])
+
+    @ocdid_state = nil
+    state = @representative.ocdid.match(/state:(\w{2})/)
+    if !state.nil?
+      @ocdid_state = state.captures[0].upcase
+    end
+
+    @default_photo = "https://twirpz.files.wordpress.com/2015/06/twitter-avi-gender-balanced-figure.png"
   end 
 end
