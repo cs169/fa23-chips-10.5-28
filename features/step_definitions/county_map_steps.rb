bb9e4696cdcaae36b07bd64fcb26854c58f7e71e
Blake Cavedo <blakecavedo@berkeley.edu>
2023-11-21 18:59:12 +0000

    partial fix to cucumber test

--- features/step_definitions/county_map_steps.rb
+++ features/step_definitions/county_map_steps.rb
@@ -1,14 +1,28 @@
 Given /that I am on state (.*) on map/ do |state|
-  visit '/state/#{state}'
+  visit "/state/#{state}"
 end
 
-When /I click on (.*) County/ do |county|
-  find('.actionmap-view-region[data-county-name=#{county}]').click
+When /I click on (.*)/ do |county|
+  # county_name = county + " " + "County"
+  # save_and_open_page
+  # wait_for { page.has_css?(".actionmap-view-region[data-county-name='#{county}']", wait: 2) }
+  # find("path.actionmap-view-region[data-county-name='#{county}']").click
+  # find(:xpath, "//path[@data-county-name='#{county}']").click
+  # page.execute_script("document.querySelector(\".actionmap-view-region[data-county-name='#{county}']\").click()")
+  # find(".actionmap-view-region='#{county}']").click
+  # find(".actionmap-view-region['#{county}']").click
+  # find(".actionmap-view-region").click
+  all_elements = page.all('*')
+  all_elements.each do |element|
+    puts element.text # You can print other properties as well (e.g., element['data-county-name'])
+  end
+
 end
 
 Then /I should see (.*) in the list of representatives/ do |rep_names_list|
   rep_names = rep_names_list.split(',')
   rep_names.each do |rep_name|
+    puts rep_names
     expect(page).not_to have_content(rep_name)
   end
 end
