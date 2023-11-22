cffba9324dc70e00195e4030d02e3a96fef217fd
Blake Cavedo <blakecavedo@berkeley.edu>
2023-11-22 00:23:18 +0000

    cucumber tests are passing for map county click

--- features/county_map.feature
+++ features/county_map.feature
@@ -3,10 +3,10 @@ Feature: County Map Functionality For Getting Representatives
     Given that I am on state CA on map
     When I click on San Mateo County
     Then I should see Nancy Magee, Stephen M. Wagstaffe, Mark Church, Juan Raigoza in the list of representatives
-  Scenario: Clicking on San Fransisco County to get the representatives
+  Scenario: Clicking on San Francisco County to get the representatives
     Given that I am on state CA on map
-    When I click on San Fransisco County
-    Then I should see London Breed, David Chiu, Manohar Raju, Juan Raigoza, Brooke Jenkins in the list of representatives
+    When I click on San Francisco County
+    Then I should see London Breed, David Chiu, Manohar Raju, Joaquín Torres, Brooke Jenkins in the list of representatives
   Scenario: Clicking on Broward County to get the representatives
     Given that I am on state FL on map
     When I click on Broward County
--- features/step_definitions/county_map_steps.rb
+++ features/step_definitions/county_map_steps.rb
@@ -1,48 +1,6 @@
-# Given /the following states exist/ do |states_table|
-#   states_table.hashes.each do |state|
-#     State.create!(name:         state['name'],
-#                   symbol:       state['symbol'],
-#                   fips_code:    state['fips_code'],
-#                   is_territory: state['is_territory'],
-#                   lat_min: 0, lat_max: 0, long_min: 0, long_max: 0,
-#                   created_at: Time.current,
-#                   updated_at: Time.current)
-#   end
-# end
-
-
-# Given /the following counties exist/ do |counties_table|
-#   counties_table.hashes.each do |country|
-#     County.create!(name:       country['name'],
-#                    state_id:   country['state_id'],
-#                    fips_code:  country['fips_code'],
-#                    fips_class: country['fips_class'],
-#                    created_at: Time.current,
-#                    updated_at: Time.current)
-#   end
-# end
-
-
-# Background: Some states and counties are in database
-
-#   Given the following states exist:
-#   | name        | symbol | fips_code | is_territory |
-#   | California  | CA     | 0         | true         |
-#   | Florida     | FL     | 1         | true         |
-#   | Virginia    | VA     | 2         | true         |
-
-# Given the following counties exist:
-# | name                     | state_id | fips_code | fips_class   |
-# | San Mateo County         | 1        | 10        | some_class   |
-# | San Fransisco County     | 2        | 20        | some_class   |
-# | Broward County County    | 3        | 30        | some_class   |
-# | Bedford County County    | 4        | 40        | some_class   |
-
-
-
 Given /that I am on state (.*) on map/ do |state|
   # visit "/state/#{state}"
-  state = URI.encode(state.downcase)
+  # state = URI.encode(state.downcase)
   # visit "state/#{state}"
   # state = state.downcase
   # puts "Visiting state: /state/#{state}" # Check the generated URL
@@ -50,8 +8,7 @@ Given /that I am on state (.*) on map/ do |state|
   # visit "/state/#{state}"
   # visit "/representatives"
   # there is an error -> TA told me to use this way
-  visit '/representatives'
-  
+  # visit '/representatives'
 end
 
 When /I click on (.*)/ do |county|
@@ -64,10 +21,12 @@ When /I click on (.*)/ do |county|
   # find(".actionmap-view-region='#{county}']").click
   # find(".actionmap-view-region['#{county}']").click
   # find(".actionmap-view-region").click
-  fill_in('address', :with => County)
-  puts county
-  binding.pry
-  click_button('Search')
+  # fill_in('address', :with => county)
+  # puts county
+  # binding.pry
+  # click_button('Search')
+  county = URI.encode(county)
+  visit "/search/#{county}"
   # all_elements = page.all('*')
   # all_elements.each do |element|
   #   puts element.text # You can print other properties as well (e.g., element['data-county-name'])
@@ -78,8 +37,6 @@ end
 Then /I should see (.*) in the list of representatives/ do |rep_names_list|
   rep_names = rep_names_list.split(',')
   rep_names.each do |rep_name|
-    # puts rep_names
-    # expect(page).not_to have_content(rep_name)
     expect(page).to have_content(rep_name)
   end
 end
