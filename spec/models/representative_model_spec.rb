1e9be0c88aa07ca881247d36009d6ab69e636d75
William Fang <willfang8@berkeley.edu>
2023-11-22 00:48:32 +0000

    Fixed other rspec test

--- spec/models/representative_model_spec.rb
+++ spec/models/representative_model_spec.rb
@@ -18,42 +18,50 @@ require 'google/apis/civicinfo_v2'
 #   end
 # end
 
+def generate_rep_info_double
+  officials_array = []
+  offices_array = []
+  (1..4).each do |n|
+    officials_array.append(generate_official)
+
+    office_to_append = instance_double(Google::Apis::CivicinfoV2::Office)
+    allow(office_to_append).to receive(:official_indices).and_return([n])
+    allow(office_to_append).to receive(:name).and_return(instance_double(String))
+    allow(office_to_append).to receive(:division_id).and_return("division #{n}")
+
+    offices_array.append(office_to_append)
+  end
+
+  rep_info_double = instance_double(Google::Apis::CivicinfoV2::RepresentativeInfoResponse)
+  allow(rep_info_double).to receive(:officials).and_return(officials_array)
+  allow(rep_info_double).to receive(:offices).and_return(offices_array)
+  rep_info_double
+end
+
+def generate_official
+  official_to_append = instance_double(Google::Apis::CivicinfoV2::Official)
+  allow(official_to_append).to receive(:address).and_return(instance_double(String))
+  allow(official_to_append).to receive(:photo_url).and_return(instance_double(String))
+  allow(official_to_append).to receive(:name).and_return(instance_double(String))
+  official_to_append
+end
+
 describe Representative do
   describe 'civic_api_to_representative_params' do
     it 'converts officials returned by civic api to Representative objects' do
       # TODO: create mock rep_info object and stub the .officials method
       # to return array of mock officials objects
-      id = 0
-      officials_array = []
-      offices_array = []
-      (1..4).each do |n|
-        official_to_append = double("official #{n}")
-        allow(official_to_append).to receive(:address).and_return(double('address'))
-        allow(official_to_append).to receive(:photo_url).and_return(double('photo'))
-        allow(official_to_append).to receive(:name).and_return(double('name'))
-        officials_array.append(official_to_append)
 
-        office_to_append = double("office #{n}")
-        allow(office_to_append).to receive(:official_indices).and_return([n])
-        allow(office_to_append).to receive(:name).and_return(double('name'))
-        allow(office_to_append).to receive(:division_id).and_return("division #{n}")
-
-        offices_array.append(office_to_append)
-      end
-
-      rep_info_double = double('info')
-      allow(rep_info_double).to receive(:officials).and_return(officials_array)
-      allow(rep_info_double).to receive(:offices).and_return(offices_array)
-
+      rep_info_double = generate_rep_info_double
       # ASSERT:
       # array size (# of officials == # of representatives)
       allow(described_class).to receive(:concatenate_addr).and_return('123 Street St, City, State, Zip')
-      allow(described_class).to receive(:find_by).and_return(double('rep'))
+      allow(described_class).to receive(:find_by).and_return(instance_double(described_class))
       result = described_class.civic_api_to_representative_params(rep_info_double)
 
-      expect(result.size).to eq(officials_array.size)
+      expect(result.size).to eq(4)
       # return object type -> NON-NULL array of representative objects
-      expect(result).to be_truthy
+      # expect(result).to be_truthy
     end
   end
 
