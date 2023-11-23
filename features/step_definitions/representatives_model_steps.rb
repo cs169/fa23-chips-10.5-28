be3c4f63b901572d5f6b6094050c35d1218676ae
William Fang <willfang8@berkeley.edu>
2023-11-23 00:18:16 +0000

    Added test for news_items show

--- features/step_definitions/representatives_model_steps.rb
+++ features/step_definitions/representatives_model_steps.rb
@@ -6,10 +6,26 @@ Given /that (.*) in database/ do |name|
     address: '1600 Pennsylvania Avenue Northwest', party: 'Democrat' })
 end
 
+Given /that (.*) has a news article/ do |name|
+  rep = Representative.where(name: name).first
+  NewsItem.create!(
+    {
+      representative: rep,
+      title:          'Newsom Orders Second Shutdown of Restaurants and Indoor Businesses amid COVID-19',
+      description:    'The new order affects 19 California counties with a surging number of coronavirus cases',
+      link:           'https://people.com/human-interest/california-gov-gavin-newsom-orders-second-shutdown-of-restaurants-and-indoor-businesses-amid-covid-19/'
+    }
+  )
+end
+
 When /I search (.*)/ do |location|
   visit '/representatives'
   fill_in('address', with: location)
   click_button('Search')
+end
+
+When /I by class click on (.*)/ do |class_name|
+  click_on(class: class_name)
 end
 
 Given /that I am on "([^"]*)"$/ do |route|
