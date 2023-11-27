e039c80edb3a3ea1167ebd524250f8c4f5c423dd
William Fang <willfang8@berkeley.edu>
2023-11-27 17:04:25 +0000

    Finished feature

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -10,17 +10,30 @@ class NewsItem < ApplicationRecord
     )
   end
 
-  def self.query_news_api (rep_name, issue)
-    #Rails.application.credentials[:NEWS_API_KEY]
-    uri = "https://newsapi.org/v2/everything?" +
-      "q=#{rep_name} AND #{issue}&" + 
-      "from=#{(Date.today - (30.days)).to_s}&" +
-      "sort_by=relevancy&" + 
-      "apiKey=#{Rails.application.credentials[:NEWS_API_KEY]}"
-      
-      uri = URI.escape(uri)
-      response = Faraday.get(uri)
-      
-      #response.body
+  # Takes string for representative name and issue
+  # Returns list of 5 articles, articles are hashes
+  def self.query_news_api(rep_name, issue)
+    uri = 'https://newsapi.org/v2/everything?' \
+          "q=#{rep_name} AND #{issue}&" \
+          "from=#{Date.today - 30.days}&" \
+          'sort_by=relevancy&' \
+          "apiKey=#{Rails.application.credentials[:NEWS_API_KEY]}"
+
+    uri = URI.escape(uri)
+    response = Faraday.get(uri)
+
+    response_articles = JSON.parse(response.body)['articles']
+    articles_list = []
+
+    5.times do |i|
+      org_article = response_articles[i]
+      article = {}
+      article['title'] = org_article['title']
+      article['link'] = org_article['url']
+      article['text'] = org_article['description']
+      articles_list[i] = article
+    end
+
+    articles_list
   end
 end
