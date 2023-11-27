e405a5e94704e19e76d5fbfe3b6f5c7c9e533c65
William Fang <willfang8@berkeley.edu>
2023-11-27 17:51:11 +0000

    Added one rspec test

--- spec/models/news_item_spec.rb
+++ spec/models/news_item_spec.rb
@@ -15,4 +15,12 @@ RSpec.describe NewsItem, type: :model do
       expect(found_news_item).to eq(news_item)
     end
   end
+
+  describe 'querying News api' do
+    it 'generates right query string' do
+      uri = NewsItem.generate_query_uri('Joe Biden', 'Abortion')
+      expect(uri).to eq('https://newsapi.org/v2/everything?q=Joe Biden AND Abortion&from=2023-10-28&sort_by=relevancy&')
+    end
+    it 'returns right list of articles'
+  end
 end
